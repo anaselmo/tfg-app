5c03b600821d1cc5cad662b234e58f0c8bdcd62a
anaselmo <nacho.moya.montoro@gmail.com>
2024-02-27 18:36:45 +0100

    alias for dir paths

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-cost = { getDefaultConfig } = require('expo/metro-config')
+const { getDefaultConfig } = require('expo/metro-config')
+const path = require('path')
 
 module.exports = (() => {
   const config = getDefaultConfig(__dirname)
@@ -12,7 +13,13 @@ module.exports = (() => {
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...resolver.sourceExts, 'svg']
+    sourceExts: [...resolver.sourceExts, 'svg'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@screens': path.resolve(__dirname, 'src/screens'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@assets': path.resolve(__dirname, 'assets')
+    }
   }
 
   return config
