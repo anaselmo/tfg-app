712fcd9bb721d194f0ddb2ea92df0b90caa46900
anaselmo <nacho.moya.montoro@gmail.com>
2024-03-12 19:48:56 +0100

    add passwordVisibility toggle to input

--- src/components/custom-input.component.tsx
+++ src/components/custom-input.component.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, type FC, useEffect, useRef } from 'react'
-import { Text, View, TextInput, type TextInputProps, Animated, Easing, type TextStyle } from 'react-native'
+import { Text, View, TextInput, type TextInputProps, Animated, Easing, type TextStyle, Pressable } from 'react-native'
 import { Controller } from 'react-hook-form'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
@@ -12,6 +12,7 @@ type CustomTextInputProps = {
   focusColor?: string
   errorColor?: string
   style?: any
+  secureTextEntry?: boolean
 } & TextInputProps
 
 const CustomTextInput: FC<CustomTextInputProps> = ({
@@ -22,16 +23,23 @@ const CustomTextInput: FC<CustomTextInputProps> = ({
   errorColor = 'red',
   label,
   style,
+  secureTextEntry = false,
   ...rest
 }) => {
   const [isFocused, setIsFocused] = useState(false)
   const [hasText, setHasText] = useState(false)
   const [error, setError] = useState(false)
   const animatedLabel = useRef(new Animated.Value(hasText || isFocused ? 1 : 0)).current
+  const [passwordVisibility, setPasswordVisibility] = useState(!secureTextEntry)
 
   useEffect(() => {
     handleBlur() // Reset the label position and size on initial render
   }, [])
+
+  const onTogglePasswordVisibility = (): void => {
+    console.log('passwordVisibility', passwordVisibility)
+    setPasswordVisibility(!passwordVisibility)
+  }
 
   const handleFocus = (): void => {
     setIsFocused(true)
@@ -78,7 +86,7 @@ const CustomTextInput: FC<CustomTextInputProps> = ({
         rules={rules}
         render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => {
           useEffect(() => {
-            setHasText(value !== '')
+            setHasText(value !== undefined && value !== '')
             setError(error !== undefined)
           }, [value, error])
           return <>
@@ -98,9 +106,22 @@ const CustomTextInput: FC<CustomTextInputProps> = ({
                   onFocus={handleFocus}
                   onBlur={handleBlur}
                   value={value}
+                  secureTextEntry={!passwordVisibility}
                   {...rest}
                   style={{ position: 'absolute', width: '100%', height: '110%', borderWidth: 0, borderColor: 'black', bottom: -7, paddingLeft: 10 }}
                 />
+                {secureTextEntry && (
+                    <Pressable
+                      onPress={onTogglePasswordVisibility}
+                      style={{ position: 'absolute', flex: 1, right: 0, marginRight: 10, padding: 10, borderWidth: 0 }}
+                    >
+                      <Ionicons
+                        name={passwordVisibility ? 'eye-off' : 'eye'}
+                        size={25}
+                        color={'#696969'}
+                      />
+                    </Pressable>
+                )}
               </View>
               {error !== undefined &&
                 <View style={{ flex: 1, flexDirection: 'row', position: 'absolute', bottom: -5, borderWidth: 0 }}>
