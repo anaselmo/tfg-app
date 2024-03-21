f6c1a48a3fdab8843b18e9e69d44c8355e560a20
anaselmo <nacho.moya.montoro@gmail.com>
2024-03-21 16:50:33 +0100

    moving getUserLocation to use it when needed in the app

---
+++ src/lib/location.ts
@@ -0,0 +1,17 @@
+import * as Location from 'expo-location'
+
+export async function getUserLocation (): Promise<{ latitude: number, longitude: number } | undefined> {
+  const { status } = await Location.requestForegroundPermissionsAsync()
+  if (status !== 'granted') {
+    alert('Permission to access location was denied')
+    return
+  }
+
+  const location = await Location.getCurrentPositionAsync({})
+  const current = {
+    latitude: location.coords.latitude,
+    longitude: location.coords.longitude
+  }
+
+  return current
+}
